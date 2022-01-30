af15e3695f57cc0c443f7de22e821bf0689d0bf2
PiotrWachowiakpl <piotrwachowiak81@gmail.com>
2022-01-30 22:27:49 +0100

    GET authors

--- src/authors/index.js
+++ src/authors/index.js
@@ -1,19 +1,63 @@
 import express from "express"
+import fs from "fs"
+import { dirname } from "path"
+import { fileURLToPath } from "url"
+import { join } from "path"
+
+const fileNameIAmWorkingOn = fileURLToPath(import.meta.url)
+const directoryNameTheFileIsIn = dirname(fileNameIAmWorkingOn)
+const authorsFilePath = join(directoryNameTheFileIsIn, "authors.json")
+
+
 
 const authorsRouter = express.Router()
 
+
+authorsRouter.post("/",async (req, res, next) => {
+    try {
+        const fileAsABuffer = fs.readFileSync(authorsFilePath)
+        const fileAsAString = fileAsABuffer.toString()
+        const fileAsAJSON = JSON.parse(fileAsAString)
+        res.send(fileAsAJSON)
+    } catch (error) {
+        res.status(500).send({message: error.message})
+        
+    }})
+    
+    
+    authorsRouter.get("/",async (req, res, next) => {
+        try {
+            
+        } catch (error) {
+            res.status(500).send({message: error.message})
+            
+        }})
+        
+        
+        authorsRouter.get("/:authorId",async (req, res, next) => {
+            try {
+                
+            } catch (error) {
+                res.status(500).send({message: error.message})
+                
+            }})
+            
+            
+            authorsRouter.put("/:authorId",async (req, res, next) => {
+                try {
+                    
+                } catch (error) {
+                    res.status(500).send({message: error.message})
+                    
+                }})
+                
+                
+                authorsRouter.delete("/:authorId",async (req, res, next) => {
+                    try {
+                        
+                    } catch (error) {
+                        res.status(500).send({message: error.message})
+                        
+    }})
+         
 export default authorsRouter
-
-authorsRouter.post("/",async (req, res, next) => {})
-
-
-authorsRouter.get("/",async (req, res, next) => {})
-
-
-authorsRouter.get("/:authorId",async (req, res, next) => {})
-
-
-authorsRouter.put("/:authorId",async (req, res, next) => {})
-
-
-authorsRouter.delete("/:authorId",async (req, res, next) => {})
--- src/server.js
+++ src/server.js
@@ -1,12 +1,14 @@
 import express from "express"
 import cors from "cors"
 import listEndpoints from "express-list-endpoints"
+import authorsRouter from "./authors/index.js"
 
 const server = express()
 const port =  3002
 
 
 server.use(express.json())
+server.use("/authors", authorsRouter)
 
 console.table(listEndpoints(server))
 server.listen(port, ()=>console.log(`Server is running on PORT : ${port}`))
