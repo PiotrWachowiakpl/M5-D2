a3fb23a98ea5db6bc87a981ce7e2031fa535b442
PiotrWachowiakpl <piotrwachowiak81@gmail.com>
2022-01-30 23:07:18 +0100

    CREATE AUTHOR

--- src/authors/index.js
+++ src/authors/index.js
@@ -1,63 +1,71 @@
-import express from "express"
-import fs from "fs"
-import { dirname } from "path"
-import { fileURLToPath } from "url"
-import { join } from "path"
+import express from "express";
+import fs, { rmSync, writeFileSync } from "fs";
+import { dirname } from "path";
+import { fileURLToPath } from "url";
+import { join } from "path";
+import uniqid from "uniqid";
 
-const fileNameIAmWorkingOn = fileURLToPath(import.meta.url)
-const directoryNameTheFileIsIn = dirname(fileNameIAmWorkingOn)
-const authorsFilePath = join(directoryNameTheFileIsIn, "authors.json")
+const fileNameIAmWorkingOn = fileURLToPath(import.meta.url);
+const directoryNameTheFileIsIn = dirname(fileNameIAmWorkingOn);
+const authorsFilePath = join(directoryNameTheFileIsIn, "authors.json");
 
+const authorsRouter = express.Router();
 
-
-const authorsRouter = express.Router()
-
-
-authorsRouter.post("/",async (req, res, next) => {
-    try {
-        const fileAsABuffer = fs.readFileSync(authorsFilePath)
-        const fileAsAString = fileAsABuffer.toString()
-        const fileAsAJSON = JSON.parse(fileAsAString)
-        res.send(fileAsAJSON)
-    } catch (error) {
-        res.status(500).send({message: error.message})
-        
-    }})
+authorsRouter.post("/", async (req, res, next) => {
+  try {
+    const { name, surname, email, dateOfBirth } = req.body;
+    const newAuthor = {
+      id: uniqid(),
+      name,
+      surname,
+      email,
+      dateOfBirth,
+      avatar: `https://ui-avatars.com/api/?name=${name}+${surname}`,
+      createdAt: new Date(),
+      updatedAt: new Date()
+    };
+    const fileAsABuffer = fs.readFileSync(authorsFilePath);
+    const fileAsAString = fileAsABuffer.toString();
+    const fileAsAJSONArray = JSON.parse(fileAsAString);
+    fileAsAJSONArray.push(newAuthor)
+    fs.writeFileSync(authorsFilePath, JSON.stringify(fileAsAJSONArray))
+    res.send(newAuthor)
     
-    
-    authorsRouter.get("/",async (req, res, next) => {
-        try {
-            
-        } catch (error) {
-            res.status(500).send({message: error.message})
-            
-        }})
-        
-        
-        authorsRouter.get("/:authorId",async (req, res, next) => {
-            try {
-                
-            } catch (error) {
-                res.status(500).send({message: error.message})
-                
-            }})
-            
-            
-            authorsRouter.put("/:authorId",async (req, res, next) => {
-                try {
-                    
-                } catch (error) {
-                    res.status(500).send({message: error.message})
-                    
-                }})
-                
-                
-                authorsRouter.delete("/:authorId",async (req, res, next) => {
-                    try {
-                        
-                    } catch (error) {
-                        res.status(500).send({message: error.message})
-                        
-    }})
-         
-export default authorsRouter
+  } catch (error) {
+    res.status(500).send({ message: error.message });
+  }
+});
+
+authorsRouter.get("/", async (req, res, next) => {
+  try {
+    const fileAsABuffer = fs.readFileSync(authorsFilePath);
+    const fileAsAString = fileAsABuffer.toString();
+    const fileAsAJSON = JSON.parse(fileAsAString);
+    res.send(fileAsAJSON);
+  } catch (error) {
+    res.status(500).send({ message: error.message });
+  }
+});
+
+authorsRouter.get("/:authorId", async (req, res, next) => {
+  try {
+  } catch (error) {
+    res.status(500).send({ message: error.message });
+  }
+});
+
+authorsRouter.put("/:authorId", async (req, res, next) => {
+  try {
+  } catch (error) {
+    res.status(500).send({ message: error.message });
+  }
+});
+
+authorsRouter.delete("/:authorId", async (req, res, next) => {
+  try {
+  } catch (error) {
+    res.status(500).send({ message: error.message });
+  }
+});
+
+export default authorsRouter;
